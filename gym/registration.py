4871f859c0cb3df95ac49ee4d0decf91671015de
Vincent Mussa <v.mussa@alustudent.com>
2026-09-10 06:55:57 +0400

    I started building register for class in GymSystem which is not my big part and added the part that finds a member by ID. Also added temporary placeholder Member, FitnessClass, and GymSystem classes so I can test my Registration code alone before merging with my team's real classes as we waiting for one member to merge all files

--- gym/registration.py
+++ gym/registration.py
@@ -20,4 +20,63 @@ class Registration:
         print(f"Registration Date: {self.registration_date}")
         print(f"Status: {self.status}")
 
+#In the program there is a chance that a gym user cancels the session , and we wont delete their other details but only the status will change
+#Creating an action that changes registration status to cancelled
+    def cancel(self):
+        self.status = "Cancelled"
+
+#I need to check if registration works perfectly so I will put class for member and all that my friends are doing I will just later delete it
+#Class for member
+class Member:
+    def __init__(self, member_id, name):
+        self.member_id = member_id
+        self.name = name
+
+#Also class for GymFitness class/session
+class FitnessClass:
+    def __init__(self, class_id, class_name, capacity):
+        self.class_id = class_id
+        self.class_name = class_name
+        self.capacity = capacity
+#So far created is a class wich contains key details of a member in a gymfitness class like the class id, class name like yoga, capacity maximum it can carry maybe we would use 20
+        self.registered_count = 0 #Right now number of registered members is 0 if one registers it increases by 1 and if one cancels it decreases by 1
+
+    def is_full(self):
+        return self.registered_count >= self.capacity
+
+    def add_registration(self):
+        if not self.is_full():
+            self.registered_count += 1
+            return True
+        else:
+            return False
+
+    def remove_registration(self): #This removes a registration and leaves one empty space
+        if self.registered_count > 0:
+            self.registered_count -= 1
+            return True
+        else:
+            return False
+
+#I also need to test how registration works with members and a class
+#Creating a simple manager to hold the members, gym classes and registrations
+class GymSystem:
+    def __init__(self):
+        self.members = []
+        self.classes = []
+        self.registrations = []
+
+#Method will help bring a member in a Fitness class
+    def register_for_class(self, member_id, gymclass_id):
+#First is checking if member_id exists
+        found_member = None
+        for member in self.members:
+            if member.member_id == member_id:
+                found_member = member
+
+        if found_member == None:
+            proint("Member ID is invalid")
+            return
+
+
 
